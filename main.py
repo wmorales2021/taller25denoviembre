5df8b6f0b8afdf18424552b3d5a8938eea2ef119
unknown <wmorales@fna.gov.co>
2023-11-24 00:25:57 -0500

    taller25denoviembre1

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm = OpenAI()
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "What is at the core of Popper's theory of science?"
+
+response = llm_chain.run(question)
+print(response)
+
